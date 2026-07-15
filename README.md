e868c691f42a717ad36ca956bf975f88442f3021
Roberto Matarazzo <seto@autistici.org>
2026-07-15 22:01:05 +0200

    First commit

---
+++ README.md
@@ -0,0 +1,3 @@
+<h1 align="center">Tokenomicon</h1>
+
+---
---
+++ tasks.py
@@ -0,0 +1,33 @@
+from invoke import task
+
+
+@task
+def format(ctx, targets="src tests"):
+    print(f"Formatting {targets}...")
+    ctx.run(f"black --quiet {targets}")
+    ctx.run(f"isort --quiet {targets}")
+    print("Done!")
+
+
+@task()
+def test(ctx, extras="--cov=wicli --cov-report=term-missing --color=yes"):
+    ctx.run(f"pytest tests {extras}")
+
+
+@task
+def clean(ctx):
+    print("Cleaning...")
+    ctx.run("rm -rf dist/ build/ .pytest_cache")
+    ctx.run('find . -name "*.egg-info" -type d -exec rm -r {} +')
+    ctx.run('find . -name "__pycache__" -type d -exec rm -r {} +')
+    print("Done!")
+
+
+@task(pre=[clean])
+def build(ctx):
+    ctx.run("python -m build")
+
+
+@task(pre=[build])
+def publish(ctx):
+    ctx.run("twine upload dist/*")
